653ea1efb2acde2002c837b4a247c82785ed7607
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-11-29 19:27:29 +0100

    added testapps

--- packages/ui/src/localService/initMockServer.ts
+++
@@ -1,4 +0,0 @@
-import mockserver from "./mockserver";
-
-mockserver.init();
-sap.ui.require(["sap/ui/core/ComponentSupport"]);
--- packages/ui/webapp/localService/initMockServer.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(["./mockserver"], function (__mockserver) {
-  function _interopRequireDefault(obj) {
-    return obj && obj.__esModule && typeof obj.default !== "undefined" ? obj.default : obj;
-  }
-  const mockserver = _interopRequireDefault(__mockserver);
-  mockserver.init();
-  sap.ui.require(["sap/ui/core/ComponentSupport"]);
-});
-//# sourceMappingURL=initMockServer.js.map
